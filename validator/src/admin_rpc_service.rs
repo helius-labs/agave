helius-labs/jito-solana

Add --public-tvu-address arg to override TVU receive address.

validator/src/admin_rpc_service.rs
@@ -265,6 +265,13 @@ pub trait AdminRpc {
         public_tpu_forwards_addr: SocketAddr,
     ) -> Result<()>;
 
+    #[rpc(meta, name = "setPublicTvuAddress")]
+    fn set_public_tvu_address(
+        &self,
+        meta: Self::Metadata,
+        public_tvu_addr: SocketAddr,
+    ) -> Result<()>;
+
     #[rpc(meta, name = "setBlockEngineConfig")]
     fn set_block_engine_config(
         &self,
@@ -867,6 +874,42 @@ impl AdminRpc for AdminRpcImpl {
                 "Public TPU Forwards addresses set to {:?} (udp) and {:?} (quic)",
                 my_contact_info.tpu_forwards(Protocol::UDP),
                 my_contact_info.tpu_forwards(Protocol::QUIC),
+            );
+            Ok(())
+        })
+    }
+
+    fn set_public_tvu_address(
+        &self,
+        meta: Self::Metadata,
+        public_tvu_addr: SocketAddr,
+    ) -> Result<()> {
+        debug!("set_public_tvu_address rpc request received: {public_tvu_addr}");
+
+        meta.with_post_init(|post_init| {
+            post_init
+                .cluster_info
+                .my_contact_info()
+                .tvu(Protocol::UDP)
+                .ok_or_else(|| {
+                    error!(
+                        "The public TVU address isn't being published. The node is likely in \
+                         repair mode. See help for --restricted-repair-only-mode for more \
+                         information."
+                    );
+                    jsonrpc_core::error::Error::internal_error()
+                })?;
+            post_init
+                .cluster_info
+                .set_tvu(public_tvu_addr)
+                .map_err(|err| {
+                    error!("Failed to set public TVU address to {public_tvu_addr}: {err}");
+                    jsonrpc_core::error::Error::internal_error()
+                })?;
+            let my_contact_info = post_init.cluster_info.my_contact_info();
+            warn!(
+                "Public TVU addresses set to {:?}",
+                my_contact_info.tvu(Protocol::UDP),
             );
             Ok(())
         })

helius-labs/agave

validator/src/admin_rpc_service.rs
@@ -267,6 +267,13 @@ pub trait AdminRpc {
         public_tpu_forwards_addr: SocketAddr,
     ) -> Result<()>;
 
+    #[rpc(meta, name = "setPublicTvuAddress")]
+    fn set_public_tvu_address(
+        &self,
+        meta: Self::Metadata,
+        public_tvu_addr: SocketAddr,
+    ) -> Result<()>;
+
     #[rpc(meta, name = "manageBlockProduction")]
     fn manage_block_production(
         &self,
@@ -752,6 +759,42 @@ impl AdminRpc for AdminRpcImpl {
                 "Public TPU Forwards addresses set to {:?} (udp) and {:?} (quic)",
                 my_contact_info.tpu_forwards(Protocol::UDP),
                 my_contact_info.tpu_forwards(Protocol::QUIC),
+            );
+            Ok(())
+        })
+    }
+
+    fn set_public_tvu_address(
+        &self,
+        meta: Self::Metadata,
+        public_tvu_addr: SocketAddr,
+    ) -> Result<()> {
+        debug!("set_public_tvu_address rpc request received: {public_tvu_addr}");
+
+        meta.with_post_init(|post_init| {
+            post_init
+                .cluster_info
+                .my_contact_info()
+                .tvu(Protocol::UDP)
+                .ok_or_else(|| {
+                    error!(
+                        "The public TVU address isn't being published. The node is likely in \
+                         repair mode. See help for --restricted-repair-only-mode for more \
+                         information."
+                    );
+                    jsonrpc_core::error::Error::internal_error()
+                })?;
+            post_init
+                .cluster_info
+                .set_tvu_socket(public_tvu_addr)
+                .map_err(|err| {
+                    error!("Failed to set public TVU address to {public_tvu_addr}: {err}");
+                    jsonrpc_core::error::Error::internal_error()
+                })?;
+            let my_contact_info = post_init.cluster_info.my_contact_info();
+            warn!(
+                "Public TVU addresses set to {:?}",
+                my_contact_info.tvu(Protocol::UDP),
             );
             Ok(())
         })